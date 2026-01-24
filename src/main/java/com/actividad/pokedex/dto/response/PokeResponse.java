83fbc224644a56398ca85db20d5ceca4fde673d1
Javi <europons@gmail.com>
2026-01-24 16:21:11 +0100

    Creada la clase con los atributos que queremos devolver del Json (los mismos que el model)

---
+++ src/main/java/com/actividad/pokedex/dto/response/PokeResponse.java
@@ -0,0 +1,17 @@
+package com.actividad.pokedex.dto.response;
+
+import com.actividad.pokedex.model.ImagenPokemon;
+import com.actividad.pokedex.model.PokemonTipos;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class PokeResponse {
+
+    private int id;
+    private String name;
+    private int height;
+    private int weight;
+    private List<PokemonTipos> types;
+    private ImagenPokemon sprites;
+}

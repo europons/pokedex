c1779b869b5dde92085983f13d1bdd8d0f1e27a9
Javi <europons@gmail.com>
2026-01-24 19:38:26 +0100

    Cambiados nombre de endpoints y añadido endopoint para mostrar la lista de 20 pokemons

--- src/main/java/com/actividad/pokedex/controller/PokemonController.java
+++ src/main/java/com/actividad/pokedex/controller/PokemonController.java
@@ -4,12 +4,13 @@ import com.actividad.pokedex.dto.request.FormularioRequest;
 import com.actividad.pokedex.model.Pokemon;
 import com.actividad.pokedex.service.PokemonService;
 import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
-@RequestMapping("/pokemon")
+@Controller
 public class PokemonController {
 
     private final PokemonService servicioPokemon;
@@ -20,14 +21,18 @@ public class PokemonController {
     
 
     @GetMapping("/buscar")
-    public String mostrarFormulario() {
+    public String mostrarFormulario(Model model) {
+        if (!model.containsAttribute("busqueda")) {
+            model.addAttribute("busqueda", new FormularioRequest());
+        }
         return "formulario";
     }
 
     @PostMapping("/buscar")
     public String buscarPokemon(@Valid @ModelAttribute("busqueda") FormularioRequest formularioRequest,
                                 BindingResult errores,
-                                Model model) {
+                                Model model,
+                                RedirectAttributes redirectAttributes) {
         if (errores.hasErrors()) {
             return "formulario";
         }
@@ -36,16 +41,38 @@ public class PokemonController {
 
         Pokemon pokemon = servicioPokemon.obtenerPokemon(nombreOId);
         if (pokemon == null) {
-            errores.rejectValue("nombreOId", "error pokemon no encontrado", "Pokémon no encontrado. Por favor, verifica el nombre o ID introducido.");
+            errores.rejectValue("nombreOId", "error", "Pokémon no encontrado. Por favor, verifica el nombre o ID introducido.");
             return "formulario";
         }
 
-        model.addAttribute("pokemon", pokemon);
-        return "redirect:/pokemon/resultado";
+        redirectAttributes.addFlashAttribute("pokemon", pokemon);
+        return "redirect:/detalles/" + nombreOId;
     }
 
-    @GetMapping("/resultado")
-    public String mostrarResultado(Model model) {
-        return "resultado";
+    @GetMapping("/detalles/{nombreOId}")
+    public String mostrarDetallesPokemon(@PathVariable("nombreOId") String nombreOId,
+                                         Model model,
+                                         RedirectAttributes redirectAttributes) {
+        if (model.getAttribute("pokemon") == null) {
+            Pokemon pokemon = servicioPokemon.obtenerPokemon(nombreOId);
+            if (pokemon == null) {
+                redirectAttributes.addFlashAttribute("error", "Pokémon no encontrado. Por favor, verifica el nombre o ID.");
+                return "redirect:/buscar";
+            }
+            model.addAttribute("pokemon", pokemon);
+        }
+        return "detallesPokemon";
+    }
+
+    @GetMapping("/listado")
+    public String mostrarListadoPokemons(Model model) {
+        var listaPokemons = servicioPokemon.obtener20Pokemons();
+        model.addAttribute("listaPokemons", listaPokemons);
+        return "listadoPokemons";
+    }
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
     }
 }

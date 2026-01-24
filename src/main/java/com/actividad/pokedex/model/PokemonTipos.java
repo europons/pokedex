a14cdcb26a236b01a2e8d6b1a6abcd794b12d609
Javi <europons@gmail.com>
2026-01-24 16:39:03 +0100

    Eliminados atributos no usados

--- src/main/java/com/actividad/pokedex/model/PokemonTipos.java
+++ src/main/java/com/actividad/pokedex/model/PokemonTipos.java
@@ -5,6 +5,5 @@ import lombok.Data;
 @Data
 public class PokemonTipos {
 
-    private int slot;
     private Tipo type;
 }
--- src/main/java/com/actividad/pokedex/model/Tipo.java
+++ src/main/java/com/actividad/pokedex/model/Tipo.java
@@ -6,5 +6,4 @@ import lombok.Data;
 public class Tipo {
 
     private String name;
-    private String url;
 }
